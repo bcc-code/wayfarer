9b707aaf1c65fc33b9f0573199fb59ceea28e5c4
Sigve Hansen <sighan12@gmail.com>
2025-10-29 13:52:35 +0100

    feat: basic ui setup

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -3,5 +3,16 @@
     <h1 class="text-2xl">
       Home
     </h1>
+    <div class="flex gap-2 mt-4">
+      <UButton variant="solid">
+        Solid Button
+      </UButton>
+      <UButton variant="soft">
+        Soft Button
+      </UButton>
+      <UButton variant="ghost">
+        Ghost Button
+      </UButton>
+    </div>
   </UContainer>
 </template>
