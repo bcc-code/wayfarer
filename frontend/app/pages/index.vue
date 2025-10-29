2ff0883e3f05ad54e24968cd477cf4291b8660e9
Sigve Hansen <sighan12@gmail.com>
2025-10-29 13:44:59 +0100

    feat: eslint

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <UContainer class="py-8">
-    <h1 class="text-2xl">Home</h1>
+    <h1 class="text-2xl">
+      Home
+    </h1>
   </UContainer>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/ui', '@nuxt/test-utils', '@nuxt/eslint',],
+  devtools: { enabled: true, },
+  css: ['~/assets/styles/main.css',],
+  ui: {
+    colorMode: false,
+  },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/test-utils', '@nuxt/eslint'],
-  css: ['~/assets/styles/main.css'],
-})
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'always',
+        quotes: 'single',
+        semi: false,
+        indent: 2,
+      },
+    },
+  },
+},)
