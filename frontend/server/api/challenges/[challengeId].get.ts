14472035c0c348698616447ec46b9569b54d1019
Sigve Hansen <sighan12@gmail.com>
2025-11-05 10:06:22 +0100

    chore: delete unneeded files

--- frontend/server/api/challenges/[challengeId].get.ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const challengeId = getRouterParam(event, 'challengeId')
-  const challenges = await $fetch('/api/challenges')
-
-  return challenges.find((challenge) => challenge.id === challengeId)
-})
--- frontend/server/api/projects/[projectId].get.ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const projectId = getRouterParam(event, 'projectId')
-  const projects = await $fetch('/api/projects')
-
-  return projects.find((project) => project.id === projectId)
-})
