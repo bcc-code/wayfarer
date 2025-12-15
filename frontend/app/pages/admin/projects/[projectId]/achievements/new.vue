43fa33c2af072cddc2e5e6bb5e19b45b471660db
Sigve Hansen <sighan12@gmail.com>
2025-12-15 15:16:48 +0100

    chore: add achievement preview to achievement new page

--- frontend/app/pages/admin/projects/[projectId]/achievements/new.vue
+++ frontend/app/pages/admin/projects/[projectId]/achievements/new.vue
@@ -29,16 +29,22 @@ const { executeMutation } = useCreateSimpleAchievementMutation()
 
 const schema = z.object({
   name: z.string().min(1, 'Name is required'),
-  description: z.string().min(1, 'Description is required'),
-  image: z.string().optional(),
+  descriptionPending: z.string().min(1, 'Description is required'),
+  descriptionCompleted: z.string().min(1, 'Description is required'),
+  notificationText: z.string().min(1, 'Notification text is required'),
+  imagePending: z.string().optional(),
+  imageCompleted: z.string().optional(),
   points: z.number().min(0, 'Points must be at least 0'),
   hidden: z.boolean(),
 })
 type Schema = z.infer<typeof schema>
 const state = reactive<Schema>({
   name: '',
-  description: '',
-  image: undefined,
+  descriptionPending: '',
+  descriptionCompleted: '',
+  notificationText: '',
+  imagePending: '',
+  imageCompleted: '',
   points: 0,
   hidden: false,
 })
@@ -107,49 +113,79 @@ async function createAchievement(event: FormSubmitEvent<Schema>) {
     </div>
     <UContainer class="py-12">
       <h1 class="mb-6 text-2xl font-bold">Create Achievement</h1>
-      <UForm
-        :state
-        :schema="schema"
-        loading-auto
-        class="flex max-w-md flex-col gap-6"
-        @submit.prevent="createAchievement"
-      >
-        <UFormField name="name" label="Name">
-          <UInput v-model="state.name" size="xl" required class="w-full" />
-        </UFormField>
-        <UFormField name="description" label="Description">
-          <UTextarea
-            v-model="state.description"
-            class="w-full"
-            autoresize
-            required
-          />
-        </UFormField>
-        <UFormField
-          name="image"
-          label="Image URL"
-          hint="(optional)"
-          help="URL to an image for this achievement"
+      <div class="grid grid-cols-2">
+        <UForm
+          :state
+          :schema="schema"
+          loading-auto
+          class="flex max-w-md flex-col gap-6"
+          @submit.prevent="createAchievement"
         >
-          <UInput v-model="state.image" size="xl" class="w-full" />
-        </UFormField>
-        <UFormField name="points" label="Points">
-          <UInput
-            v-model.number="state.points"
-            type="number"
-            size="xl"
-            required
-            class="w-full"
-          />
-        </UFormField>
-        <UFormField name="hidden" label="Hidden">
-          <UCheckbox
-            v-model="state.hidden"
-            label="Hide this achievement from users until they earn it"
-          />
-        </UFormField>
-        <UButton type="submit" size="lg" block>Create Achievement</UButton>
-      </UForm>
+          <UFormField name="name" label="Name">
+            <UInput v-model="state.name" size="xl" required class="w-full" />
+          </UFormField>
+          <UFormField name="descriptionPending" label="Description (Pending)">
+            <UTextarea
+              v-model="state.descriptionPending"
+              class="w-full"
+              autoresize
+              required
+            />
+          </UFormField>
+          <UFormField
+            name="descriptionCompleted"
+            label="Description (Completed)"
+          >
+            <UTextarea
+              v-model="state.descriptionCompleted"
+              class="w-full"
+              autoresize
+              required
+            />
+          </UFormField>
+          <UFormField name="notificationText" label="Notification text">
+            <UTextarea
+              v-model="state.notificationText"
+              class="w-full"
+              autoresize
+              required
+            />
+          </UFormField>
+          <UFormField
+            name="imagePending"
+            label="Image URL (Pending)"
+            hint="(optional)"
+            help="URL to an image for this achievement"
+          >
+            <UInput v-model="state.imagePending" size="xl" class="w-full" />
+          </UFormField>
+          <UFormField
+            name="imageCompleted"
+            label="Image URL (Completed)"
+            hint="(optional)"
+            help="URL to an image for this achievement"
+          >
+            <UInput v-model="state.imageCompleted" size="xl" class="w-full" />
+          </UFormField>
+          <UFormField name="points" label="Points">
+            <UInput
+              v-model.number="state.points"
+              type="number"
+              size="xl"
+              required
+              class="w-full"
+            />
+          </UFormField>
+          <UFormField name="hidden" label="Hidden">
+            <UCheckbox
+              v-model="state.hidden"
+              label="Hide this achievement from users until they earn it"
+            />
+          </UFormField>
+          <UButton type="submit" size="lg" block>Create Achievement</UButton>
+        </UForm>
+        <AdminAchievementPreview :achievement="state" />
+      </div>
     </UContainer>
   </div>
 </template>
