2a88d0d28fc8fe63659f0f7e82935b089f6c2fbc
Sigve Hansen <sighan12@gmail.com>
2026-02-05 11:20:56 +0100

    wip: local admin stats

---
+++ frontend/app/pages/admin/my-church/statistics.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'church-admin',
+  middleware: ['admin'],
+})
+</script>
+
+<template>
+  <div>
+    <div class="border-default border-b py-2">
+      <UContainer>
+        <UBreadcrumb
+          :items="[
+            {
+              label: $t('admin.breadcrumb.home'),
+              to: { name: 'admin-my-church' },
+            },
+            {
+              label: $t('admin.churchHome.statistics'),
+            },
+          ]"
+        />
+      </UContainer>
+    </div>
+    <UContainer class="py-6">
+      <!-- Stats here -->
+    </UContainer>
+  </div>
+</template>
