7fa491b65c741d450a48881ad15900d23a788acd
Sigve Hansen <sighan12@gmail.com>
2026-02-06 12:54:05 +0100

    wip(admin): gamenight page

---
+++ frontend/app/pages/admin/my-church/gamenight-[gamenight].vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'church-admin',
+  middleware: ['admin'],
+})
+
+const route = useRoute('admin-my-church-gamenight-gamenight')
+const gamenight = computed(() => route.params.gamenight)
+</script>
+
+<template>
+  <div>
+    <div class="border-default border-b py-2">
+      <UContainer>
+        <UBreadcrumb
+          :items="[
+            {
+              label: $t('admin.breadcrumb.home'),
+              to: { name: 'admin-my-church' },
+            },
+            {
+              label: $t('admin.churchHome.gameNights'),
+              to: { name: 'admin-my-church-gamenights' },
+            },
+            {
+              label: $t('admin.churchHome.gameNight', { number: gamenight }),
+            },
+          ]"
+        />
+      </UContainer>
+    </div>
+    <UContainer class="py-6 relative">
+      <UButton
+        color="neutral"
+        variant="soft"
+        size="lg"
+        :to="{ name: 'admin-my-church' }"
+      >
+        <Icon name="lucide:arrow-left" />
+        {{ $t('admin.common.back') }}
+      </UButton>
+
+      <h1 class="text-3xl font-semibold mt-12">
+        {{ $t('admin.churchHome.gameNight', { number: gamenight }) }}
+      </h1>
+    </UContainer>
+  </div>
+</template>
