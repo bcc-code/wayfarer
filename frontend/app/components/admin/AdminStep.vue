5eeff7c275407d4d4ae7c7ec812e28f2deaa4464
Sigve Hansen <sighan12@gmail.com>
2026-02-16 10:46:15 +0100

    wip: start work on gamenight admin panel

---
+++ frontend/app/components/admin/AdminStep.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps<{
+  step: number
+  title: string
+  description?: string
+  active?: boolean
+}>()
+</script>
+
+<template>
+  <section
+    :id="`step-${step}`"
+    :data-active="active"
+    class="grid grid-cols-[75px_1fr] gap-4 items-baseline py-12 data-[active=false]:pointer-events-none data-[active=false]:opacity-50"
+  >
+    <div>
+      <p
+        :data-active="active"
+        class="text-2xl rounded-full grid place-items-center bg-accented aspect-square size-12 data-[active=true]:bg-primary data-[active=true]:text-neutral-900"
+      >
+        {{ step }}
+      </p>
+    </div>
+    <div>
+      <h2 class="text-2xl font-bold mb-2">{{ title }}</h2>
+      <p v-if="description" class="text-muted">
+        {{ description }}
+      </p>
+      <div class="mt-6">
+        <slot />
+      </div>
+    </div>
+  </section>
+</template>
--- frontend/app/pages/admin/my-church/gamenight-[gamenight].vue
+++ frontend/app/pages/admin/my-church/gamenight-[gamenight].vue
@@ -54,17 +54,54 @@ const { data: cryptexUrl, status: cryptexUrlStatus } = useFetch<{
         {{ $t('admin.common.back') }}
       </UButton>
 
-      <h1 class="text-3xl font-semibold mt-12">
+      <h1 class="text-3xl font-semibold my-12">
         {{ $t('admin.churchHome.gameNight', { number: gamenight }) }}
       </h1>
 
-      <UButton
-        :loading="cryptexUrlStatus !== 'success'"
-        size="xl"
-        :to="cryptexUrl?.url"
-      >
-        Gå til Game Night kontrollpanel
-      </UButton>
+      <h2 class="text-2xl font-semibold mb-4">Før du starter</h2>
+      <ul class="space-y-4">
+        <li>
+          <UCheckbox
+            label="Pass på at alle brukere har installert Interact appen"
+          />
+        </li>
+      </ul>
+
+      <div class="divide-y divide-muted mb-24">
+        <AdminStep :step="1" title="Vis logo på storskjermen"> </AdminStep>
+        <AdminStep :step="2" title="Spill av film 1"> </AdminStep>
+        <AdminStep
+          :step="3"
+          title="Start tippingen"
+          description="Alle må gå inn på 'Utfordringer' siden i Interact appen"
+        >
+          <UButton size="xl">Start tipping</UButton>
+        </AdminStep>
+        <AdminStep :step="4" title="Spill av film 2"> </AdminStep>
+        <AdminStep :step="5" title="Send ut login-kode til pc26.bcc.media">
+        </AdminStep>
+        <AdminStep :step="6" title="Gå til adminsiden for å starte spillet">
+          <UButton
+            :loading="cryptexUrlStatus !== 'success'"
+            size="xl"
+            :to="cryptexUrl?.url"
+            trailing-icon="lucide:arrow-right"
+          >
+            Gå til adminsiden for spillet
+          </UButton>
+        </AdminStep>
+        <div class="py-12 pl-22.75 text-2xl font-semibold">
+          <p>Fullfør steg 7 til 11 på den andre siden</p>
+        </div>
+        <AdminStep :step="12" title="Spill av film 3"> </AdminStep>
+        <AdminStep :step="13" title="Gi deltakerne poeng for tippingen">
+          <UButton size="xl">Frigi poeng nå</UButton>
+        </AdminStep>
+        <div class="py-24 text-center text-2xl font-semibold">
+          <p>Du er nå ferdig! 🎉</p>
+          <p>Game Night kan avsluttes.</p>
+        </div>
+      </div>
     </UContainer>
   </div>
 </template>
